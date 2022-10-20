53c29c9ed4db8fb9f7ca27a7f0e50d3fbf230367
Malik A-Abdul <mabdul@noos.fr>
2022-10-20 12:50:20 +0200

    Ajout de express limit rate sur les routes de users

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
         console.log(req.path);
         const token = req.headers.authorization.split(' ')[1];
         console.log('auth / token : '+token); // -------------------- debug ----------------
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 3, // limite chaque IP à 3 requêtes per windowMs
+    message: "Too many requests, please try again after 15 minutes"});
+
+router.post('/signup', limiter, userCtrl.signup);
+router.post('/login', limiter, userCtrl.login);
 
 
 module.exports = router;
